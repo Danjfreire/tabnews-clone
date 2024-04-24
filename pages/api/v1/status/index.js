176151f44de00c67191c2bb79be14f5a28beaf38
Daniel Freire <danjfreire@gmail.com>
2024-04-24 08:25:36 -0300

    feat: add status endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,10 +1,32 @@
 import database from "infra/database.js";
 
 async function status(req, res) {
-  const result = await database.query("SELECT 1+1 as sum;");
-  console.log(result.rows);
+  const updatedAt = new Date().toISOString();
 
-  res.status(200).json({ response: "this is a response" });
+  const dbVersionResult = await database.query("SHOW server_version;");
+  const dbVersionValue = dbVersionResult.rows[0].server_version;
+
+  const dbMaxConnectionsResult = await database.query("SHOW max_connections;");
+  const dbMaxConnections = dbMaxConnectionsResult.rows[0].max_connections;
+
+  const DB_NAME = process.env.POSTGRES_DB;
+  const dbOpenConnectionsResult = await database.query({
+    text: "SELECT COUNT(*) FROM pg_stat_activity WHERE datname=$1;",
+    values: [DB_NAME],
+  });
+
+  const dbOpenConnections = dbOpenConnectionsResult.rows[0].count;
+
+  res.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: dbVersionValue,
+        max_connections: parseInt(dbMaxConnections),
+        open_connections: parseInt(dbOpenConnections),
+      },
+    },
+  });
 }
 
 export default status;
